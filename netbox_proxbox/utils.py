a9b8ede821ea1d3bb9b979347245cea40687f478
emersonfelipesp <emersonfelipe.2003@gmail.com>
2025-03-07 15:08:42 +0000

    Move 'get_fastapi_url' function to utils module and add REQUESTS_CA_BUNDLE os environment variable if needed

---
+++ netbox_proxbox/utils.py
@@ -0,0 +1,44 @@
+def get_fastapi_url(object):
+    domain_or_ip = None
+    if object.domain:
+        domain_or_ip = object.domain
+    if object.ip_address:
+        domain_or_ip = str(object.ip_address).split('/')[0]
+    
+    
+    # Define HTTP(S) URL for FastAPI
+    fastapi_url_https = f"https://{domain_or_ip}:{object.port}"
+    fastapi_url_http = f"http://{domain_or_ip}:{object.port}"
+    fastapi_url = fastapi_url_https if object.verify_ssl else fastapi_url_http
+    
+    # Define (Secure) WebSocket URL for FastAPI
+    fastapi_wss_url = f"wss://{domain_or_ip}:{object.port}/ws"
+    fastapi_ws_url = f"ws://{domain_or_ip}:{object.port}/ws"
+    fastapi_websocket_url = fastapi_wss_url if object.verify_ssl else fastapi_ws_url
+    
+    if 'proxbox.backend.local' in fastapi_url:
+        # If proxbox.backend.local is in the URL, set the REQUESTS_CA_BUNDLE environment variable.
+        # It means user used mkcert to generate a certificate for the domain.
+        # This is necessary to avoid SSL errors, so that python requests library can trust the certificate.
+        
+        import os
+        import subprocess
+        
+        try:
+            # Run mkcert -CAROOT to get the root certificate path.
+            ca_root_folder = subprocess.run(['mkcert', '-CAROOT'], capture_output=True, text=True, check=True).stdout.strip()
+            
+            os.environ['REQUESTS_CA_BUNDLE'] = f'/{ca_root_folder}/rootCA.pem'
+        except subprocess.CalledProcessError as e:
+            print(f'Error running mkcert -CAROOT: {e}')
+        
+        except Exception as e:
+            print(f'Error setting REQUESTS_CA_BUNDLE: {e}\nLikely because the user did not use mkcert to generate a certificate.')
+    
+    return {
+        'domain': object.domain,
+        'ip_address': object.ip_address,
+        'http_url': fastapi_url,
+        'websocket_url': fastapi_websocket_url,
+        'verify_ssl': object.verify_ssl
+    }
